3141ff48d08d5d445a2c9cb263558b3cf6559b20
SparkVRG <sparkvrg@yandex.ru>
2023-04-04 18:39:07 +0300

    Header developed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function navSubmenuToggle() {
+    let button = document.getElementsByClassName('header__nav-item-submenu-list')[0];
+
+    if (button.style.display == 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
+function burgerMenuToggle() {
+    let button = document.getElementsByClassName('header__nav-mobile-list')[0];
+
+    if (button.style.display == 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
