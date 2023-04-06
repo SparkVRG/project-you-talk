275449025a90f0a25775b1da2c9f1d43381cfffc
SparkVRG <sparkvrg@yandex.ru>
2023-04-06 20:15:27 +0300

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ function navSubmenuToggle() {
     }
 }
 
+function navMobileSubmenuToggle() {
+    let button = document.getElementsByClassName('header__nav-mobile-item-submenu-list')[0];
+
+    if (button.style.display == 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
 function burgerMenuToggle() {
     let button = document.getElementsByClassName('header__nav-mobile-list')[0];
 
